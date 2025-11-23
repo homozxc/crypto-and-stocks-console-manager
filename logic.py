302de603a9add38993e2c23507919862dd2a160f
putilinandrey <putilinandreyv@yandex.ru>
2025-11-23 20:18:05 +0300

    Test commit

--- logic.py
+++ logic.py
@@ -1,19 +1,14 @@
 def load_portfolio(path):
-
-
+    return 1
 def get_price(asset):
     price = 0
     return price
 
 def show_statistics():
-    
-
+    return 1
 def add_asset(asset):
-    
-
+    return 1
 def swap_asset(asset1, asset2):
-
-
-
+    return 1
 def get_graph(path):
-    
+    return 1
