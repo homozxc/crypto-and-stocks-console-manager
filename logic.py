e565b3dd09ebaca8f01df0ca5c883eaf8d5c7867
alexhom32 <whoiam789000@gmail.com>
2025-12-05 12:45:11 +0300

    add export_history_data

--- logic.py
+++ logic.py
@@ -1,14 +1,5 @@
-def load_portfolio(path):
-    return 1
-def get_price(asset):
-    price = 0
-    return price
-
-def show_statistics():
-    return 1
-def add_asset(asset):
-    return 1
-def swap_asset(asset1, asset2):
-    return 1
-def get_graph(path):
-    return 1
+def export_history_data(data):
+    print("\n--- History Export ---")
+    print("Date,Action,Cash")
+    for entry in data["history"]:
+        print(f"{entry['date']},{entry['action']},{entry['cash_snapshot']}")
