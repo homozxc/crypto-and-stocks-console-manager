af0bc84d4f5f91d0132dd09c84ae67ab24cb5b89
putilinandrey <putilinandreyv@yandex.ru>
2025-12-05 12:04:07 +0300

    Added get_real_price

--- logic.py
+++ logic.py
@@ -1,14 +1,44 @@
-def load_portfolio(path):
-    return 1
-def get_price(asset):
-    price = 0
-    return price
+import json
+import datetime
+import yfinance
 
-def show_statistics():
-    return 1
-def add_asset(asset):
-    return 1
-def swap_asset(asset1, asset2):
-    return 1
-def get_graph(path):
-    return 1
+WATCHLIST = {
+    "BTC-USD": "Bitcoin",
+    "ETH-USD": "Ethereum",
+    "SOL-USD": "Solana",
+    "XRP-USD": "XRP",
+    "DOGE-USD": "Dogecoin",
+    "AAPL": "Apple Inc.",
+    "TSLA": "Tesla Inc.",
+    "MSFT": "Microsoft Corp.",
+    "AMZN": "Amazon.com",
+    "NVDA": "NVIDIA Corp."
+}
+
+def get_real_price(ticker:str):
+    '''
+    Принимает на вход строку с названием тикера из словаря WATCHLIST5.
+
+    Используя библиотеку yfinance, получает цену закрытия тикера за
+    последний торговый день.
+
+    Возвращает цену закрытия тикера в формате float. Если входная переменная содержит
+    пустую строку или во время выполнения кода возникает ошибка, возвращает None
+
+    :param: str ticker: название тикера из списка WATCHLIST
+    :return: Цена тикера за последний торговый день if not data.empty else None
+    :rtype: float
+
+    :example:
+
+    >>> 'BTC-USD'
+    >>> get_real_price('BTC-USD')
+    90000.00001
+    '''
+
+    print(f'Fetching the price of {ticker}...')
+    try:
+        data = yfinance.Ticker(ticker).history(period='1d')
+        return data['Close'].iloc[-1] if not data.empty else None
+    except:
+        return None
